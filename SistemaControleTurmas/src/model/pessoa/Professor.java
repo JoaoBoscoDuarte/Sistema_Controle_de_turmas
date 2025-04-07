c26b5ab741bf5cf7121f859754d8444a378f83a2
randerson <randerson.soares@dcx.ufpb.br>
2025-04-07 00:36:37 -0300

    Adicionando classe professor

---
+++ SistemaControleTurmas/src/model/pessoa/Professor.java
@@ -0,0 +1,45 @@
+package model.pessoa;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Professor extends Pessoa{
+
+    private List<ArrayList> turmasMinistradas = new ArrayList<>();
+    private LocalDate data;
+
+    public Professor(String nome, String telefone, String email, List<ArrayList> turmasMinistradas) throws Exception {
+        super(nome, telefone, email);
+        this.turmasMinistradas = turmasMinistradas;
+        LocalDate.now();
+
+    }
+
+    public List<ArrayList> getTurmasMinistradas() {
+        return turmasMinistradas;
+    }
+
+    public void setTurmasMinistradas(List<ArrayList> turmasMinistradas) {
+        this.turmasMinistradas = turmasMinistradas;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+}
