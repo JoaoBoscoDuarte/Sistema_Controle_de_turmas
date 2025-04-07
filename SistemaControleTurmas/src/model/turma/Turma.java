93846307a4e9a167b03bef9059afe9ed0d62ae53
JoaoBoscoDuarte <joaoboscoduartef@gmail.com>
2025-04-07 00:39:20 -0300

    Criação da classe turma inicial

--- SistemaControleTurmas/src/model/turma/Turma.java
+++ SistemaControleTurmas/src/model/turma/Turma.java
@@ -1,4 +1,75 @@
 package model.turma;
 
+import model.pessoa.Aluno;
+import model.disciplina.Disciplina;
+import model.pessoa.Professor;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
 public class Turma {
+
+    private Disciplina nomeDisciplina;
+    private Professor nomeProfessor;
+    private Map unidadesNotasAlunos; ///????????????????????????????
+    private int capacidadeMaximaAlunos;
+    private int capacidadeMinimaAlunos;
+    private int qtdUnidadesAvaliativas;
+    private LocalDate dataCriacaoTurma;
+    private List<Aluno> alunos;
+    private codigoTurma;
+
+    public Turma(Disciplina nomeDisciplina, Professor nomeProfessor, Map unidadesNotasAlunos, int capacidadeMaximaAlunos, int capacidadeMinimaAlunos, int qtdUnidadesAvaliativas) {
+        this.nomeDisciplina = nomeDisciplina;
+        this.nomeProfessor = nomeProfessor;
+        this.unidadesNotasAlunos = unidadesNotasAlunos;
+        this.capacidadeMaximaAlunos = capacidadeMaximaAlunos;
+        this.capacidadeMinimaAlunos = capacidadeMinimaAlunos;
+        this.qtdUnidadesAvaliativas = qtdUnidadesAvaliativas;
+        this.dataCriacaoTurma = LocalDate.now();
+    }
+
+    public int getCapacidadeMaximaAlunos() {
+        return capacidadeMaximaAlunos;
+    }
+
+    public void setCapacidadeMaximaAlunos(int capacidadeMaximaAlunos) {
+        this.capacidadeMaximaAlunos = capacidadeMaximaAlunos;
+    }
+
+    public int getCapacidadeMinimaAlunos() {
+        return capacidadeMinimaAlunos;
+    }
+
+    public void setCapacidadeMinimaAlunos(int capacidadeMinimaAlunos) {
+        this.capacidadeMinimaAlunos = capacidadeMinimaAlunos;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Turma turma = (Turma) o;
+        return capacidadeMaximaAlunos == turma.capacidadeMaximaAlunos && capacidadeMinimaAlunos == turma.capacidadeMinimaAlunos && qtdUnidadesAvaliativas == turma.qtdUnidadesAvaliativas && Objects.equals(nomeDisciplina, turma.nomeDisciplina) && Objects.equals(nomeProfessor, turma.nomeProfessor) && Objects.equals(unidadesNotasAlunos, turma.unidadesNotasAlunos) && Objects.equals(dataCriacaoTurma, turma.dataCriacaoTurma) && Objects.equals(alunos, turma.alunos);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nomeDisciplina, nomeProfessor, unidadesNotasAlunos, capacidadeMaximaAlunos, capacidadeMinimaAlunos, qtdUnidadesAvaliativas, dataCriacaoTurma, alunos);
+    }
+
+    @Override
+    public String toString() {
+        return "Turma{" +
+                "Nome da Disciplina = " + nomeDisciplina +
+                ", Nome Professor = " + nomeProfessor +
+                ", Unidade de Notas dos alunos = " + unidadesNotasAlunos +
+                ", Capacidade Máxima de alunos = " + capacidadeMaximaAlunos +
+                ", Capacidade Mínima de alunos = " + capacidadeMinimaAlunos +
+                ", Quantidade de Unidades Avaliativas = " + qtdUnidadesAvaliativas +
+                ", Data de criação da turma = " + dataCriacaoTurma +
+                ", Alunos = " + alunos +
+                '}';
+    }
 }
