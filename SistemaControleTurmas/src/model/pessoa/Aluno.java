8b58988a2aa3eec920c4c643e95cea1886f96a66
annaAlves <anna.alves@dcx.ufpb.br>
2025-04-12 22:59:41 -0300

    atualizando

--- SistemaControleTurmas/src/model/pessoa/Aluno.java
+++ SistemaControleTurmas/src/model/pessoa/Aluno.java
@@ -1,59 +1,85 @@
 package model.pessoa;
 
 import java.time.LocalDate;
-import java.util.Objects;
 
 public class Aluno extends Pessoa {
-    private String matricula;
-    private LocalDate dataCriacao;
-    private static int contaMatricula = 1;
-
     public Aluno(String nome, String telefone, String email) throws Exception {
         super(nome, telefone, email);
-        this.matricula = geradorDeMatricula();
-        this.dataCriacao = LocalDate.now();
     }
 
-    private String geradorDeMatricula() {
-        int anoMatricula = LocalDate.now().getYear();
-        int numeroMatricula = contaMatricula++;
-        return anoMatricula + "" + contaMatricula;
+    @Override
+    public String getNome() {
+        return super.getNome();
     }
 
+    @Override
+    public void setNome(String nome) throws PessoaInvalidaException {
+        super.setNome(nome);
+    }
+
+    @Override
+    public String getTelefone() {
+        return super.getTelefone();
+    }
+
+    @Override
+    public void setTelefone(String telefone) throws PessoaInvalidaException {
+        super.setTelefone(telefone);
+    }
+
+    @Override
+    public String getEmail() {
+        return super.getEmail();
+    }
+
+    @Override
+    public void setEmail(String email) throws PessoaInvalidaException {
+        super.setEmail(email);
+    }
+
+    @Override
+    public boolean isAtivo() {
+        return super.isAtivo();
+    }
+
+    @Override
+    public void setAtivo(boolean ativo) {
+        super.setAtivo(ativo);
+    }
+
+    @Override
     public String getMatricula() {
-        return matricula;
+        return super.getMatricula();
     }
 
-    public void setMatricula(String matricula) {
-        this.matricula = matricula;
-    }
-
+    @Override
     public LocalDate getDataCriacao() {
-        return dataCriacao;
+        return super.getDataCriacao();
     }
 
+    @Override
     public void setDataCriacao(LocalDate dataCriacao) {
-        this.dataCriacao = dataCriacao;
+        super.setDataCriacao(dataCriacao);
+    }
+
+    @Override
+    public void validar() {
+        super.validar();
     }
 
     @Override
     public String toString() {
-        return "Aluno{" +
-                "matricula='" + matricula + '\'' +
-                ", dataCriacao=" + dataCriacao +
-                '}';
+        return super.toString();
     }
 
     @Override
     public boolean equals(Object o) {
-        if (o == null || getClass() != o.getClass()) return false;
-        if (!super.equals(o)) return false;
-        Aluno aluno = (Aluno) o;
-        return Objects.equals(matricula, aluno.matricula) && Objects.equals(dataCriacao, aluno.dataCriacao);
+        return super.equals(o);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(super.hashCode(), matricula, dataCriacao);
+        return super.hashCode();
     }
+
 }
