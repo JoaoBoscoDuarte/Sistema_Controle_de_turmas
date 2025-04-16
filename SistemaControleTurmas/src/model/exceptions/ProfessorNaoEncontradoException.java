0b7cb04e56232fb18d8c8956410d2bdc196b8a0e
randerson <randerson.soares@dcx.ufpb.br>
2025-04-16 11:08:29 -0300

    atualizando ProfessorNaoException

--- SistemaControleTurmas/src/model/exceptions/ProfessorNaoEncontradoException.java
+++ SistemaControleTurmas/src/model/exceptions/ProfessorNaoEncontradoException.java
@@ -1,7 +1,13 @@
 package model.exceptions;
 
 public class ProfessorNaoEncontradoException extends Exception {
+
+    public ProfessorNaoEncontradoException(){
+        super("Professor não encontrado ou se encontra desativado.");
+    }
+
     public ProfessorNaoEncontradoException(String message) {
         super(message);
     }
+
 }
