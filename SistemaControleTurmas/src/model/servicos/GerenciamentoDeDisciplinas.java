51cb187ec6815b61ab575968c01dc752334d6b89
Usuário <michaellendario123@gmail.com>
2025-04-26 12:57:32 -0300

    Add

--- SistemaControleTurmas/src/model/servicos/GerenciamentoDeDisciplinas.java
+++ SistemaControleTurmas/src/model/servicos/GerenciamentoDeDisciplinas.java
@@ -22,7 +22,7 @@ public class GerenciamentoDeDisciplinas implements Serializable {
     }
 
     // Método que cadastra uma nova disciplina ---------------------------------------------> OK
-    public void cadastraDisciplina(String nome, int cargaHoraria) throws DisciplinaJaCadastradaException, CargaHorariaInvalidaException, DisciplinaInvalidaException {
+    public void cadastraDisciplina(String nome, int cargaHoraria) throws DisciplinaJaCadastradaException, CargaHorariaInvalidaException, NomeDaDisciplinaInvalidoException, DisciplinaInvalidaException {
         validaNomeDisciplina(nome);
 
         if (cargaHoraria <= 0) {
